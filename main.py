eae487e6a4b6c696b76c3eec557381cc9144c71f
markodmaki123 <marko.dobrijevic@student.um.si>
2021-06-07 19:10:27 +0200

    Added face image comparison and saving known face to file.

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import cv2
 import tkinter.filedialog as file
 from pymongo import MongoClient
-from bson.objectid import ObjectId
+import face_recognition
+import numpy as np
 
 
 client = MongoClient("mongodb+srv://root:jinxisthebest@cluster0.1jnpn.mongodb.net/sample%95airbnb?retryWrites=true&w=majority")
 
-def get(post_id):
+
+def get(post_id): # getting an object from a database based on its id
  global client
  db = client.get_database('sample_airbnb')
  coll = db.get_collection('listingsAndReviews')
  post = coll.find_one({'_id': post_id})
- name = post['name']
- print(name)
+ name = post['name'] # getting the name of the given object
+ print(name) # printing the name
 
 # Get user supplied values
 filename = file.askopenfilename(initialdir="/Users/KAB-27-3/PycharmProjects/FaceRwithFile", title="Select file",
@@ -25,7 +27,7 @@ faceCascade = cv2.CascadeClassifier(cascPath)
 
 # Read the image
 image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # changed gray to rgb
 
 detected = []
 # Detect faces in the image
@@ -39,18 +41,32 @@ faces = faceCascade.detectMultiScale(
 if faces is not None:
     detected.append(faces)
 
-print("Found {0} faces!".format(len(faces)))
-get("10006546")
+print("Found {0} faces!".format(len(faces))) # printing how many faces are found
+get("10006546") # getting a collection from database with this id
 
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces:
+    face = image[y: y + w, x: x + h]
+    cv2.imwrite("foundface.jpg", face)
     cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+faceFile = "0_face.jpg"
+faceImage = face_recognition.load_image_file("0_face.jpg")
+faceEncoding = face_recognition.face_encodings(faceImage)[0]
+
+faceToCompareFile = "foundface.jpg"
+faceToCompare = face_recognition.load_image_file("foundface.jpg")
+faceToCompareEncoding = face_recognition.face_encodings(faceToCompare)[0]
+
+results = face_recognition.compare_faces([faceEncoding], faceToCompareEncoding)
+
+print(results)
 
 for face in detected:
     i = 0
     add = str(i)
-    cv2.imwrite(add+"_face.jpg", gray[y: y + w, x: x + h])
+    #cv2.imwrite(add+"_face.jpg", image[y: y + w, x: x + h]) # saving found face to file
     i += 1
 
-cv2.imshow("Faces found", image)
+cv2.imshow("Faces found", image) # displaying the face
 cv2.waitKey(0)
