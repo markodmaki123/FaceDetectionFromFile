090c6f83a67858a7ac8b746aa6ab816dbb65b29d
markodmaki123 <marko.dobrijevic@student.um.si>
2021-06-07 15:03:10 +0200

    Connected mongoDB for taking pictures out of the database and scaning them.

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import cv2
 import tkinter.filedialog as file
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+
+client = MongoClient("mongodb+srv://root:jinxisthebest@cluster0.1jnpn.mongodb.net/sample%95airbnb?retryWrites=true&w=majority")
+
+def get(post_id):
+ global client
+ db = client.get_database('sample_airbnb')
+ coll = db.get_collection('listingsAndReviews')
+ post = coll.find_one({'_id': post_id})
+ name = post['name']
+ print(name)
 
 # Get user supplied values
 filename = file.askopenfilename(initialdir="/Users/KAB-27-3/PycharmProjects/FaceRwithFile", title="Select file",
@@ -18,7 +31,7 @@ detected = []
 # Detect faces in the image
 faces = faceCascade.detectMultiScale(
     gray,
-    scaleFactor=1.2,
+    scaleFactor=1.1,
     minNeighbors=5,
     minSize=(30, 30),
 )
@@ -27,6 +40,7 @@ if faces is not None:
     detected.append(faces)
 
 print("Found {0} faces!".format(len(faces)))
+get("10006546")
 
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces:
